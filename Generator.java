875e4b171f32b85d112c41629fdfc871be33377d
Niko Ziozis <NZiozis@users.noreply.github.com>
2017-06-06 14:06:02 -0400

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -2,22 +2,30 @@ import java.util.Hashtable<K,V>;
 
 
 public class Generator{
-
-    private String _output;
  
 
-    public static int getK(){
-	return _k;
-    }
-	
-    private static String getRandomWord(){
+    private static String getRandomWord(Markov m){
 	// return a random word generated from the array of a given Markov
     }
     
-    public static String genParagraph(int k, Hashtable<K,V> hash){
+    public static String generate(int k, Hashtable<K,V> hash, String [] beginning, int length){
+	String [] output = new String[length];
+
+	
+
+	for (int i = 0; i < beginning.length; i++)
+	    output[i] = beginning[i];
+
+	int counter = k;
+	
+	counter - k --> counter
+
+
+	for (int i = k + 1; i < length; i++)
+	    output[i] = getRandomWord(Markov m);
+
 	return _output;
     }
 
 }
-
 
