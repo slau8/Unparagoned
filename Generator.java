f972920ef317462fd0b1a52573e7d046765c256a
Shannon Lau <shannonlau00@gmail.com>
2017-06-07 15:04:11 -0400

    updated from folder

--- Generator.java
+++ Generator.java
@@ -1,9 +1,6 @@
 import java.util.*;
-<<<<<<< HEAD
 
 public class Generator{
-=======
->>>>>>> 86da53fa90b13fc64a5ec7ed13433c5040e5ea7c
 
     private static String getRandomWord(Markov m){
 	// return a random word generated from the array of a given Markov
@@ -15,11 +12,7 @@ public class Generator{
 	return m.getArray().get(index);
 
     }
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 86da53fa90b13fc64a5ec7ed13433c5040e5ea7c
     public static String generate(int k, Hashtable<String,Markov> hash, String [] beginning, int length){
 	String [] output = new String[length];
 	for (int i = 0; i < beginning.length; i++)
@@ -33,32 +26,17 @@ public class Generator{
 		prevK += output[back] + " ";
 		back++;
 	    }
-<<<<<<< HEAD
 	    Markov m = hash.get(prevK);     //not too sure how to deal with probability here
 	    output[counter] = getRandomWord(m);
 	}
 
-=======
-	    Markov m = hash.get(prevK);     
-	    output[counter] = getRandomWord(m);
-	}
-    
->>>>>>> 86da53fa90b13fc64a5ec7ed13433c5040e5ea7c
 	String end = "";
 	for (String s: output)
 	    end += ( s + " ");
 	end = end.substring(0, end.length()-1);
-<<<<<<< HEAD
 
 
 	return end;
     }
 
-=======
-		
-	
-	return end;
-    }
-  
->>>>>>> 86da53fa90b13fc64a5ec7ed13433c5040e5ea7c
 }
