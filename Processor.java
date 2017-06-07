f39eb27da8387a69cbb307972f494a9727b13a10
snyarko99 <snyarko99@gmail.com>
2017-06-07 13:51:05 -0400

    Debugging1

--- Processor.java
+++ Processor.java
@@ -1,27 +1,24 @@
-import java.util.Hashtable<K,V>;
+import java.util.*;
 
 public class Processor{
 
-  public static Hashtable<K,V> process(int k, String[] arr){
+  public static Hashtable<String,Markov> process(int k, String[] arr){
     Hashtable<String,Markov> hash = new Hashtable<String,Markov>();
     for (int i = 0; i < arr.length - k; i++){
-      String element;
+      String element = "";
       for (int j = 0; j < k; j++){
         element += arr[i+j] + " ";
       }
-      if (!containsKey(element)){
+      if (!hash.containsKey(element)){
         Markov m = new Markov(element);
-        hash.put(x,m);
+        hash.put(element,m);
         m.add(arr[i+k]);
       }
       else {
         hash.get(element).add(arr[i+k]);
       }
     }
+    return hash;
   }
-
+    
 }
-
-  /*public static String generate(int k, Hashtable<K,V> hash){
-
-  }*/
