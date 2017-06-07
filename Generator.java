cc075e2fa65300e3a79a5314347e7c46ebda6ef3
Tanim Ahmed <tahmed3@stuy.edu>
2017-06-07 07:38:42 -0400

    Update Generator.java

--- Generator.java
+++ Generator.java
@@ -1,31 +1,33 @@
 import java.util.Hashtable<K,V>;
 
-
-public class Generator{
- 
-
-    private static String getRandomWord(Markov m){
-	// return a random word generated from the array of a given Markov
+public class Generator{  
+  
+  private static String getRandomWord(Markov m){
+    // return a random word generated from the array of a given Markov
+    
+    //im not sure how to make it get a RANDOM element
+    
+  }
+  
+  public static String generate(int k, Hashtable<K,V> hash, String [] beginning, int length){
+    String [] output = new String[length];
+    for (int i = 0; i < beginning.length; i++)
+      output[i] = beginning[i];
+    
+//   uses previous k words to generate a random output
+    for (int counter = k; counter < length; counter++){
+      int back = counter - k;
+      String prevK = "";
+      while (back < counter){
+        prevK += output[back] + " ";
+        back++;
+      }
+      Markov m = Hashtable.get(prevK);     //not too sure how to deal with probability here
+      output[counter].getRandomWord(m);
     }
     
-    public static String generate(int k, Hashtable<K,V> hash, String [] beginning, int length){
-	String [] output = new String[length];
-
-	
-
-	for (int i = 0; i < beginning.length; i++)
-	    output[i] = beginning[i];
-
-	int counter = k;
-	
-	counter - k --> counter
-
-
-	for (int i = k + 1; i < length; i++)
-	    output[i] = getRandomWord(Markov m);
-
-	return _output;
-    }
-
+    
+    return output;
+  }
+  
 }
-
