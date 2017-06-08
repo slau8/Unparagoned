0b08afe39e685b6444390d0e3399592e1000017b
Niko Ziozis <NZiozis@users.noreply.github.com>
2017-06-07 22:50:24 -0400

    Create ReadFile.java
    Min working version

--- ReadFile.java
+++ ReadFile.java
@@ -29,30 +29,4 @@ public class ReadFile{
         return null; // if not a legit file, scanner will ask for another file name
 	return readFile(textFile).split(" ");
     }
-
-
-    //############# This was to test and see how the methods worked inside buffered read #########################
-
-    /*public static void main(String[] args) {
-
-      String[] parts; // this is an array with all the words in the input file in it separated by spaces. This means that there are still things like punctuation and capital letters in it. I don't think that this is an issue
-      String sOutput = "";// this is the string that holds the entire text file
-      try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
-      String sCurrentLine;
-      while ((sCurrentLine = br.readLine()) != null) {
-      sCurrentLine =  sCurrentLine.trim();
-      sOutput += sCurrentLine;
-      }
-      } catch (IOException e) {
-      e.printStackTrace();
-      }
-
-      parts = sOutput.split(" ");
-
-      //########### This code was to make sure that we are getting the desired output ###########
-      //System.out.println(sOutput);
-      //System.out.println("\n\n\n");
-      //System.out.println(toString(parts));
-      }*/
-
 }
