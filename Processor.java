73700e08fd6cc135a94e06375980c29f344883af
Niko Ziozis <NZiozis@users.noreply.github.com>
2017-06-07 22:49:21 -0400

    Create Processor.java
    Min working version

--- Processor.java
+++ Processor.java
@@ -2,23 +2,30 @@ import java.util.*;
 
 public class Processor{
 
-  public static Hashtable<String,Markov> process(int k, String[] arr){
-    Hashtable<String,Markov> hash = new Hashtable<String,Markov>();
-    for (int i = 0; i < arr.length - k; i++){
-      String element = "";
-      for (int j = 0; j < k; j++){
-        element += arr[i+j] + " ";
-      }
-      if (!hash.containsKey(element)){
-        Markov m = new Markov(element);
-        hash.put(element,m);
-        m.add(arr[i+k]);
-      }
-      else {
-        hash.get(element).add(arr[i+k]);
-      }
+    public static Hashtable<String,Markov> process(int k, String[] arr){
+      
+	Hashtable<String,Markov> hash = new Hashtable<String,Markov>();
+    
+	for (int i = 0; i < arr.length; i++){
+	    String element = "";
+      
+      
+	    for (int j = 0; j < k; j++){
+		element += arr[ (i+j) % arr.length] + " ";
+	    }
+
+      
+	    if (!hash.containsKey(element)){
+		Markov m = new Markov(element);
+		hash.put(element,m);
+		m.add(arr[ (i+k) % arr.length ]);
+	    }  
+	    else {
+		hash.get(element).add(arr[ (i+k) % arr.length ]);
+	    }
+	}
+
+	return hash;
     }
-    return hash;
-  }
 
 }
