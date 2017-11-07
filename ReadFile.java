3e03452bed8e4a1296d26230ba503cb0ed7a4860
Shannon Lau <shannonlau00@gmail.com>
2017-11-07 15:29:17 -0500

    organized text files and processes

--- ReadFile.java
+++ ReadFile.java
@@ -1,27 +1,29 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
 
 public class ReadFile{
 
     public static String readFile(String textFile){
-	String sOutput = "";// this is the string that holds the entire text file
+    	String sOutput = "";// this is the string that holds the entire text file
+      String filePath = new File("").getAbsolutePath();
+      filePath = filePath + "/Library/" + textFile + ".txt";
+    	try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 
-	try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
+    		String sCurrentLine;
 
-		String sCurrentLine;
+    		while ((sCurrentLine = br.readLine()) != null) {
+    		    sCurrentLine =  sCurrentLine.trim(); // gets rid of leading and ending spaces and newlines
+    		    sOutput += sCurrentLine;
+    		}
 
-		while ((sCurrentLine = br.readLine()) != null) {
-		    sCurrentLine =  sCurrentLine.trim(); // gets rid of leading and ending spaces and newlines
-		    sOutput += sCurrentLine;
-		}
+    	    } catch (IOException e) {
+            return null; // returns null if not found for the Scanner to respond appropriately
+          //e.printStackTrace();
+    	}
 
-	    } catch (IOException e) {
-        return null; // returns null if not found for the Scanner to respond appropriately
-      //e.printStackTrace();
-	}
-
-	return sOutput;
+    	return sOutput;
     }
 
     public static String[] readFileArray(String textFile){
